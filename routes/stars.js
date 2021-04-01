626f513820c08a8f489fc13f54ce9ec6d609d5f3
shivaramkothapally <shivaramkothapally177@gmail.com>
2021-03-31 21:14:43 -0500

    adding images

---
+++ routes/stars.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var nodeConsole = require('console');
+const { table } = require('console');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    let query = req.query
+    console.log(`rows ${query.rows}`);
+    console.log(`cols ${query.cols}`);
+    var rows = Number(query.rows);
+    var cols = Number(query.cols);
+    res.render('stars', { title: 'Stars', query: query });
+
+});
+
+module.exports = router;
